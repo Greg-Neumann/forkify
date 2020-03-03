60d31a8103a12e44cc445211c8ef19b479a5dc3f
Greg on the iMac <greginthenorth@outlook.com>
2020-03-03 15:10:21 +0000

    Limit recipe title to one line with  whole words

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -16,9 +16,34 @@ export const clearResults = () => {
     elements.searchResultsList.innerHTML = ''; /* you clear rendered HTML !*/
 };
 /*
+Utility function, used within this module, to keep the recipe title as whole words on one line
+By Default, the character limit is set to 17.
+*/
+const limitRecipeTitle = (title, charLimit = 17) => {
+    /* 
+    We're going to build the new title up char-by-char until whole words do not exceed the limit 
+    */
+    const newTitle = []; 
+    if(title.length > charLimit) {
+        title.split(' ').reduce((currentTitleLength,word)=> {
+            if(currentTitleLength + word.length <= charLimit) {
+                /*
+                The current count, plus this word, is less than the line-limit
+                */
+               newTitle.push(word);
+            }
+            return currentTitleLength + word.length /* Advance to end of included word */
+        },0); /* start the reduce with currentTitleLength = 0 */
+        return `${newTitle.join(' ')} ...`
+    } else {
+        return title
+    }
+};
+/*
 Utility function, used just within this module, to render the current recipe into the DOM
 */
 const renderRecipe = recipe => {
+
     const markUP = `
         <li>
             <a class="results__link" href="#${recipe.recipe_id}">
@@ -26,7 +51,7 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
